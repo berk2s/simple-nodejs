8140aee7b4e50691a0850008ad2a847bfeb2ab26
berk2s <berktopcu@outlook.com>
2020-03-31 12:59:33 +0300

    ready for production

--- helper/fcm.js
+++ helper/fcm.js
@@ -1,3 +1,3 @@
 const fcm = require('fcm-notification');
-const FCM = new fcm('./mavideniste-firebase.json');
+const FCM = new fcm('./mavideniste-ios-push-firebase-adminsdk-68hcq-f5c2ce3ab1.json');
 module.exports = FCM;
