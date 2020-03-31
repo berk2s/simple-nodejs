e86027319f9202491d0ebdae052fbe880a50b083
berk2s <berktopcu@outlook.com>
2020-03-31 08:46:43 +0300

    ready for production

--- constants/config.js
+++ constants/config.js
@@ -2,7 +2,7 @@ module.exports = {
     API_KEY: 'c7da07af-9c40-4125-9a88-e4a5b59f5305',
     PANEL_API_KEY: '56595339-71a8-46e6-a890-700620d6a9ae',
     TOPIC_EVERYBODY: '_Herkes_',
-    PANEL_URL: 'https://www.mavideniste.com/panel',
+    PANEL_URL: 'https://www.mavideniste.com',
     SMS_API_ID:'API414053853366',
     SMS_API_PASSWORD:'u"9R6(Utf+$eB%4g',
     TIERED_START: 568245
