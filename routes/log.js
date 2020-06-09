8cc2a0d02c03664d806b87404839acf3f3694ebe
berk2s <berktopcu@outlook.com>
2020-06-09 16:01:14 +0300

    heroku

--- routes/log.js
+++ routes/log.js
@@ -19,21 +19,34 @@ router.get('/between/:back', cors(corsOptions), async (req, res, next) => {
     try{
         const { back } = req.params;
         const moment = require('moment-timezone');
-        const startDate_TypeDate = new Date();
-        const finishDate_TypeDate= new Date();
-        startDate_TypeDate.setDate(new Date().getDate()-(back))
-        startDate_TypeDate.setHours(new Date().getHours()-(new Date().getHours()))
-        finishDate_TypeDate.setDate(new Date().getDate())
-        const { _d : startDate} = moment.tz(new Date(startDate_TypeDate), "Europe/Istanbul");
-        const { _d : finishDate} = moment.tz(finishDate_TypeDate, "Europe/Istanbul");
-        const result = await Log.find({
-            created_at: {
-                $gte:startDate, // >=
-                $lte:finishDate // <=
-            }
-        });
+        const resultLog = [0, 0, 0, 0, 0, 0, 0];
+
+        for(let i = 0; i<7; i++) {
+            const startDate_TypeDate = new Date();
+            const finishDate_TypeDate = new Date();
+            startDate_TypeDate.setDate(new Date().getDate() - (i))
+            startDate_TypeDate.setHours(0)
+            startDate_TypeDate.setMinutes(0)
+            startDate_TypeDate.setSeconds(0)
+            startDate_TypeDate.setMilliseconds(0)
+            finishDate_TypeDate.setDate(new Date().getDate() - (i))
+            finishDate_TypeDate.setHours(23)
+            finishDate_TypeDate.setMinutes(59)
+            finishDate_TypeDate.setSeconds(59)
+            finishDate_TypeDate.setMilliseconds(59)
+            const {_d: startDate} = moment.tz(new Date(startDate_TypeDate), "Europe/Istanbul");
+            const {_d: finishDate} = moment.tz(finishDate_TypeDate, "Europe/Istanbul");
+            const result = await Log.find({
+                created_at: {
+                    $gte: startDate, // >=
+                    $lte: finishDate // <=
+                }
+            });
+            resultLog[6-i] = result.length;
+        }
+
         res.json({
-            data:result
+            data:resultLog
         });
     }catch(e){
         res.json(e);
