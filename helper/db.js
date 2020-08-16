d8ad9f11dff781e5ece71bb7af60bfd0fc1a2e6e
berk2s <berktopcu@outlook.com>
2020-08-16 20:49:39 +0300

    db change

--- helper/db.js
+++ helper/db.js
@@ -4,7 +4,7 @@ module.exports = () => {
 
 //    mongoose.connect('mongodb://mavideniste_dba:jvA6kCDZ!8CGy98@ds259111.mlab.com:59111/heroku_4lcm1msg', {  useUnifiedTopology: true , useNewUrlParser: true } )
 
-    mongoose.connect('mongodb://heroku_4lcm1msg:7ifgc966h2mrfcb23t7g9414p2@ds259111.mlab.com:59111/heroku_4lcm1msg', {  useUnifiedTopology: true , useNewUrlParser: true } )
+   // mongoose.connect('mongodb://heroku_4lcm1msg:7ifgc966h2mrfcb23t7g9414p2@ds259111.mlab.com:59111/heroku_4lcm1msg', {  useUnifiedTopology: true , useNewUrlParser: true } )
 //nslookup -type=SRV _mongodb._tcp.cluster-4lcm1msg.4ute3.mongodb.net
 //mongodb://heroku_4lcm1msg:7ifgc966h2mrfcb23t7g9414p2@ds259111.mlab.com:59111/heroku_4lcm1msg
     mongoose.connection
