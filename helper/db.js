97440c4b013ed5e4f157a45c4abe62e0aa570dcf
berk2s <berktopcu@outlook.com>
2020-08-16 20:40:41 +0300

    db change

--- helper/db.js
+++ helper/db.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose');
 
 module.exports = () => {
 
-    mongoose.connect('mongodb://mavideniste_dba:jvA6kCDZ!8CGy98@ds259111.mlab.com:59111/heroku_4lcm1msg', {  useUnifiedTopology: true , useNewUrlParser: true } )
+//    mongoose.connect('mongodb://mavideniste_dba:jvA6kCDZ!8CGy98@ds259111.mlab.com:59111/heroku_4lcm1msg', {  useUnifiedTopology: true , useNewUrlParser: true } )
 
+    mongoose.connect('mongodb://heroku_4lcm1msg:7ifgc966h2mrfcb23t7g9414p2@ds259111.mlab.com:59111/heroku_4lcm1msg', {  useUnifiedTopology: true , useNewUrlParser: true } )
+//nslookup -type=SRV _mongodb._tcp.cluster-4lcm1msg.4ute3.mongodb.net
+//mongodb://heroku_4lcm1msg:7ifgc966h2mrfcb23t7g9414p2@ds259111.mlab.com:59111/heroku_4lcm1msg
     mongoose.connection
         .on('open', () => {
             console.log('MongoDB connection is succesfull!')
@@ -12,3 +15,5 @@ module.exports = () => {
             console.log(err);
         })
 }
+
+
