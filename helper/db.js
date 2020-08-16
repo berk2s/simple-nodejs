ab83fdc59e68206ee51872549020c5b2cfa14e3d
berk2s <berktopcu@outlook.com>
2020-08-16 21:24:06 +0300

    db change last

--- helper/db.js
+++ helper/db.js
@@ -4,9 +4,18 @@ module.exports = () => {
 
 //    mongoose.connect('mongodb://mavideniste_dba:jvA6kCDZ!8CGy98@ds259111.mlab.com:59111/heroku_4lcm1msg', {  useUnifiedTopology: true , useNewUrlParser: true } )
 
-   // mongoose.connect('mongodb://heroku_4lcm1msg:7ifgc966h2mrfcb23t7g9414p2@ds259111.mlab.com:59111/heroku_4lcm1msg', {  useUnifiedTopology: true , useNewUrlParser: true } )
-//nslookup -type=SRV _mongodb._tcp.cluster-4lcm1msg.4ute3.mongodb.net
-//mongodb://heroku_4lcm1msg:7ifgc966h2mrfcb23t7g9414p2@ds259111.mlab.com:59111/heroku_4lcm1msg
+    //mongodb+srv://mavideniste_dba:jvA6kCDZ!8CGy98@cluster-4lcm1msg.4ute3.mongodb.net/heroku_4lcm1msg?retryWrites=true&w=majority
+
+    mongoose.connect('mongodb+srv://mavideniste_dba:jvA6kCDZ!8CGy98@cluster-4lcm1msg.4ute3.mongodb.net/heroku_4lcm1msg?retryWrites=true&w=majority', {  useUnifiedTopology: true , useNewUrlParser: true } )
+
+
+    //nslookup -type=SRV _mongodb._tcp.cluster-4lcm1msg.4ute3.mongodb.net
+    //mongodb://heroku_4lcm1msg:7ifgc966h2mrfcb23t7g9414p2@ds259111.mlab.com:59111/heroku_4lcm1msg
+
+    // last one
+
+    //mongodb+srv://mavideniste_dba:jvA6kCDZ!8CGy98@cluster-4lcm1msg.4ute3.mongodb.net/heroku_4lcm1msg?retryWrites=true&w=majority
+
     mongoose.connection
         .on('open', () => {
             console.log('MongoDB connection is succesfull!')
